dcec44cc3b546442a752a192fd7181c15c7036ba
Stendhalsynd <qmflf556@naver.com>
2023-08-28 13:03:41 +0900

    try websocket

--- m8/w9/websocket/index.js
+++ m8/w9/websocket/index.js
@@ -1,7 +1,6 @@
 const ws = require("ws");
 const PORT = 8000;
 const express = require("express");
-const e = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -14,15 +13,25 @@ const server = app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
 
-const wss = new ws.Server({ port: PORT });
-
+// 웹소켓 서버 접속
+const wss = new ws.Server({ server });
+// 브라우저(클라이언트)들을 담을 변수선언
+const sockets = [];
+// socket 변수는 접속한 브라우저
 wss.on("connection", (socket) => {
   console.log("클라이언트가 연결되었습니다.");
+  // sockets 배열에 브라우저 추가
+  sockets.push(socket);
 
   socket.on("message", (message) => {
     console.log(`클라이언트로부터 받은 메시지: ${message}`);
-
-    socket.send(`서버메시지: ${message}`);
+    // 클라이언트로 응답 메시지 전송
+    // socket.send(`서버메시지: ${message}`);
+    const { name, msg } = JSON.parse(message);
+    console.log("name, msg : ", name, msg);
+    sockets.forEach((socket) => {
+      socket.send(`${message}`);
+    });
   });
   // 오류
   socket.on("error", (err) => {
